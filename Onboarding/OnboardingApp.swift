06103ff2aa558dd4b1682990e0f33c4a274c3c42
Kody Deda <kodydeda4@gmail.com>
2023-05-22 07:41:53 -0400

    Initial Commit

---
+++ Onboarding/OnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingApp.swift
+//  Onboarding
+//
+//  Created by Kody Deda on 5/22/23.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
